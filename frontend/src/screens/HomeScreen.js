230c72faf6a73d4918686653b70b8874a31bd62e
Ricardo Flores <rik.fm1809@gmail.com>
2020-11-19 18:12:36 -0500

    update 1911

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { data } from '../data';
 import { Product } from '../components/Product';
 
 export const HomeScreen = () => {
+
+    const [products, setProducts] = useState([]);
+
     return (
         <div className="row center">
             {
