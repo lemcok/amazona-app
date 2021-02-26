0c1894d59522b144a3868332c67669d35496977d
Ricardo Flores <rik.fm1809@gmail.com>
2021-02-26 17:09:03 -0500

    add productDetails constants, actions, reducers and api on server

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
 
 export const listProducts = async(dispatch) => {
     dispatch({
@@ -7,9 +7,32 @@ export const listProducts = async(dispatch) => {
     });
     try {
         const {data}  = await Axios.get('/api/products');
-        console.log(data)
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (err) {
         dispatch({type: PRODUCT_LIST_FAIL, payload: err.message})
     }
 }
+
+
+export const detailsProduct = ( productId ) => {
+    return async( dispatch ) => {
+        dispatch( { type: PRODUCT_DETAILS_REQUEST, payload: productId } );
+        try {
+            const {data} = await Axios.get(`/api/products/${ productId }`);
+            console.log(data)
+            dispatch({
+                type: PRODUCT_DETAILS_SUCCESS,
+                payload: data
+            })
+        } catch (err) {
+            dispatch({
+                type: PRODUCT_DETAILS_FAIL,
+                payload: 
+                    err.response && err.response.data.message 
+                        ? err.response.data.message
+                        : err.message,
+            })
+        }
+    }
+}
+
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { productsReducer } from '../reducers/productsReducer';
+import { productDetailsReducer, productsReducer } from '../reducers/productsReducer';
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
     products: productsReducer,
+    productDetails: productDetailsReducer
 })
 
 export const store = createStore(
