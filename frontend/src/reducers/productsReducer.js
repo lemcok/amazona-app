aa8e5559cf1b0adc375cd897f1964bbb884323b0
Ricardo Flores <rik.fm1809@gmail.com>
2021-02-26 15:13:13 -0500

    implement actions, constants and reducers of products

--- frontend/src/reducers/productsReducer.js
+++ frontend/src/reducers/productsReducer.js
@@ -1,16 +1,23 @@
-import { types } from "../types/types";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+// import { types } from "../types/types";
 
 
-const initialState = {};
+const initialState = { loading: true, products: [] };
 
 export const productsReducer = ( state = initialState, action ) => {
     switch ( action.type ) {
-        case types.notesLoad:
+        case PRODUCT_LIST_REQUEST:
+            return { loading: true }
+        case PRODUCT_LIST_SUCCESS:
             return {
-                ...state,
-                products: [ action.payload, ...state.products ] 
+                loading: false, products: action.payload
             }
-    
+        case PRODUCT_LIST_FAIL:
+            return{
+                loadind: false,
+                error: action.payload
+            }
+
         default:
             return state;
     }
