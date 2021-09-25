618f820fe55e9dc2926ba0adc1434bb005e27e39
Ricardo Flores <rik.fm1809@gmail.com>
2021-09-25 11:26:46 -0500

    Create Place Order Api

--- backend/utils.js
+++ backend/utils.js
@@ -1,5 +1,5 @@
 import userRouter from "./routers/userRouter.js"
-import jwt from 'jsonwebtoken'
+import jwt, { decode } from 'jsonwebtoken'
 
 export const generateToken = ( user ) => {
     return jwt.sign({
@@ -12,4 +12,21 @@ export const generateToken = ( user ) => {
             expiresIn: '30d'
         }
     )
+}
+
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(authorization){
+        const token = authorization.slice(7, authorization.length);
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+            if (err){
+                res.status(401).send({ message: 'Invalid Token' })
+            }else {
+                req.user = decode;
+                next();
+            }
+        })
+    }else {
+        res.status(401).send({ message: 'No Token' })
+    }
 }
